bb3ddb4e79f92a4147cb2e34446aaf81487884c4
mistryhiren1992 <mistryhiren1992@gmail.com>
2022-05-23 21:06:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,598 @@
+<html lang="en">
+<head>
+<title>Buy Organic Seeds, Dry Fruits &amp; Nuts from Soultatva</title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, minimal-ui" />
+
+
+<link rel="icon" type="image/png" href="favicon.png">
+<link rel="shortcut icon" href="favicon.ico">
+
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+
+
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+
+
+<link href="css/jquery.bxslider.css" rel="stylesheet" type="text/css" />
+	
+ <link rel="stylesheet" type="text/css" href="css/slick.css">
+  <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+	
+<link href="css/common-text.css" rel="stylesheet" type="text/css" />
+<link href="css/common-layout.css" rel="stylesheet" type="text/css" />
+<link href="css/menu.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/resrponsive.css">
+<link rel="stylesheet" href="css/jquery.fancybox.min.css" type="text/css" media="screen" />
+<link href="css/jquery.bxslider.css" rel="stylesheet" type="text/css" />
+
+
+</head>
+
+<body>
+	
+    
+    <a href="javascript:void(0);" id="Arrowfix" style="display: none;"><span></span></a>
+    
+	
+	
+	
+	<!--TOPMAIN-->
+	<div class="topmain">
+		<div class="topmainleft">
+			<select name="langues" id="langues">
+			  <option value="English">English</option>
+			  <option value="Hindi">Hindi</option>
+			  <option value="Gujarati">Gujarati</option>
+			  <option value="Marathi">Marathi</option>
+			</select>
+		</div>
+		<div class="topmainright">
+			<a href="">My Wishlist</a> <span>|</span>
+			<a href="">Sign in</a> <span>|</span>
+			<a href="">Register</a> <span>|</span>
+			<a href="">Store Locator</a> <span>|</span>
+			<a href="">Blog</a>
+		</div>
+	</div>
+	<!--TOPMAIN-->
+    
+	
+	
+	<!--TOPMAIN Wrapper-->
+	<div class="topmainwrapper">
+		<div class="topmaincol1"><a href="index.php"><img src="images/soultatva-logo1.png" alt="" ></a></div>
+		<div class="topmaincol2">
+			<input type="text" placeholder="Search for products" ><button>Search</button>
+		</div>
+		<div class="topmaincol3">
+			<div class="group clearboth">
+				<h6><a href="tel:Call 1300 000 XXX">Call 1300 000 XXX</a></h6>
+				<h5><a href="#">Cart <i class="fas fa-bell"></i> <span>0</span></a></h5>
+			</div>
+		</div>
+	</div>
+	<!--TOPMAIN-->
+	
+	
+	<!--Menu-->
+	
+	<div class="menubg">
+		<nav>
+			<a href="#"><i class="fa fa-bars fa-2x"></i></a>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About US</a></li>
+				<li><a href="#">Products</a></li>
+				<li><a href="#">Our Pillars</a></li>
+				<li><a href="#">Recipes</a></li>
+				<li><a href="#">Blogs</a></li>
+				<li><a href="#">Contact us</a></li>
+			</ul>
+		</nav>
+	</div>
+	
+	<!--END-->
+	
+	
+	
+	<!--Header Wrapper-->
+	
+	<div class="headerwrapper">
+		<ul class="nobullet headerslider">
+			<li><img src="images/header.jpg" alt="" ></li>
+			<li><img src="images/header.jpg" alt="" ></li>
+		</ul>
+		
+	</div>
+	
+	<!--END-->
+	
+	
+	
+	<!--Header Bottom  Wrapper-->
+	
+	<div class="bg1">
+		<div class="containwrapper tac">
+			<div class="headerbotbox"><img src="images/header-bootom-banner1.jpg" alt="" ></div>
+			<div class="headerbotbox"><img src="images/header-bootom-banner2.jpg" alt="" ></div>
+			<div class="headerbotbox"><img src="images/header-bootom-banner1.jpg" alt="" ></div>
+		</div>
+	</div>
+	
+	<!--END-->
+	
+	
+	<!--Contain Wrapper-->
+	
+	<div class="containwrapper">
+		<h3>Tranding</h3>
+		<ul class="nobullet assetsslide">
+			<li>
+				<div class="conbox1">
+					<a href="#">
+						<img src="images/tranding1.jpg" alt="" >
+					</a>
+					<div class="conbox1con trandingheight">
+						<h6>Sunflower Seeds Roasted Salted</h6>
+						<h5><span>₹420</span><i>₹599</i></h5>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="conbox1">
+					<a href="#">
+						<img src="images/tranding1.jpg" alt="" >
+					</a>
+					<div class="conbox1con trandingheight">
+						<h6>Sunflower Seeds Roasted Salted</h6>
+						<h5><span>₹420</span><i>₹599</i></h5>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="conbox1">
+					<a href="#">
+						<img src="images/tranding1.jpg" alt="" >
+					</a>
+					<div class="conbox1con trandingheight">
+						<h6>Sunflower Seeds Roasted Salted</h6>
+						<h5><span>₹420</span><i>₹599</i></h5>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="conbox1">
+					<a href="#">
+						<img src="images/tranding1.jpg" alt="" >
+					</a>
+					<div class="conbox1con trandingheight">
+						<h6>Sunflower Seeds Roasted Salted</h6>
+						<h5><span>₹420</span><i>₹599</i></h5>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="conbox1">
+					<a href="#">
+						<img src="images/tranding1.jpg" alt="" >
+					</a>
+					<div class="conbox1con trandingheight">
+						<h6>Sunflower Seeds Roasted Salted</h6>
+						<h5><span>₹420</span><i>₹599</i></h5>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="conbox1">
+					<a href="#">
+						<img src="images/tranding1.jpg" alt="" >
+					</a>
+					<div class="conbox1con trandingheight">
+						<h6>Sunflower Seeds Roasted Salted</h6>
+						<h5><span>₹420</span><i>₹599</i></h5>
+					</div>
+				</div>
+			</li>
+			
+		</ul>
+		
+		
+	</div>
+	
+	<!--END-->
+	
+	
+	
+	<!--Contain Wrapper-->
+	
+	<div class="bg1">
+		<div class="containwrapper">
+			<h3>Featured Products</h3>
+			<ul class="nobullet assetsslide">
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+		
+		<div class="containwrapper">
+			<h3>Recommended for you</h3>
+			<ul class="nobullet assetsslide">
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="conbox2">
+						<a href="#">
+							<img src="images/tranding1.jpg" alt="" >
+						</a>
+						<div class="conbox2con trandingheight">
+							<h6>Sunflower Seeds Roasted Salted</h6>
+							<h5><span>₹420</span><i>₹599</i></h5>
+							<div class="btn1"><a href="#">Add to cart</a></div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	
+	<!--END-->
+	
+	
+	<!--Contain Wrapper-->
+	<div class="containwrapper footertoptext">
+		<h6>The One-stop Shopping Destination</h6>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		
+		<h6>Where does it come from?</h6>
+		<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+		<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+		
+		
+		<h6>Why do we use it?</h6>
+		<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+		
+		
+		<h6>Where can I get some?</h6>
+		<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+		
+	</div>
+	<!--END-->
+	
+	
+	
+	<!--Footer Wrapper-->
+	<div class="footerbg">
+		<div class="footerwrapper">
+			<div class="group clearboth">
+				<div class="footercol1">
+					<h6>Quick Links</h6>
+					<a href="">Blog</a>
+					<a href="">OUR STORY</a>
+					<a href="">ETERNALLY PURE</a>
+					<a href="">PROCESS</a>
+					<a href="">OUR PILLARS</a>
+					<a href="">RECIPES</a>
+					<a href="">FAQs</a>
+				</div>
+				<div class="footercol1">
+					<h6>PRODUCTS</h6>
+					<a href="">SEED</a>
+					<a href="">NUT</a>
+					<a href="">COMBOS</a>
+					<a href="">SPECIAL PACKS</a>
+				</div>
+				<div class="footercol1">
+					<h6>LEGAL</h6>
+					<a href="">TERMS &amp; CONDITIONS</a>
+					<a href="">PAYMENT POLICY</a>
+					<a href="">CONTACT US</a>
+				</div>
+				<div class="footercol2">
+					<h6>Subscribe to our Newsletter</h6>
+					<div class="group clearboth footersearch">
+						<input type="text" placeholder="Your Email ID" ><button>Search</button>
+					</div>
+					<hr>
+					
+				</div>
+			</div>
+			<hr>
+			<div class="group clearboth">
+				<div class="footerbottomleft"> 
+					Copyright &copy; 2022 Soultatva. All Right Reserved | Developed By <a href="" target="_blank">Jency Software</a>
+				</div>
+				<div class="footerbottomright">
+					<div class="footersocial">
+						<a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
+                        <a href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--END-->
+	
+	
+	
+    
+ 
+
+<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="js/ajax-1.9.0.min.js"></script>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 50) {
+				$('#Arrowfix').fadeIn();
+			} else {
+				$('#Arrowfix').fadeOut();
+			}
+		});
+		$('#Arrowfix').click(function(){
+			$("html, body").animate({ scrollTop: 0 }, 400);
+			return false;
+		});
+	});
+</script>
+
+<script src="js/jquery.balance.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$(window).load(function() {
+		$('.trandingheight').balance() ;
+		$('.assetsboxheight').balance() ;
+		$('.idxvideoheight').balance() ;
+	});
+</script>
+	
+	
+<script src="js/jquery.bxslider.js" type="text/javascript"></script>	
+<script type="text/javascript">
+	
+	$(document).ready(function(){
+	  if($(window).width() < 30500){
+		  $('.headerslider').bxSlider({
+			infiniteLoop:true,
+			auto:true,
+			pager:false,
+			controls:true,
+			  oneToOneTouch:true,
+				pause: 8000,
+				touchEnabled: true,
+		 });
+	  }
+	});
+	
+	$(document).ready(function(){
+	  if($(window).width() < 30500){
+		  $('.bannerslider').bxSlider({
+			infiniteLoop:true,
+			auto:true,
+			pager:false,
+			controls:false,
+			  oneToOneTouch:true,
+				pause: 8000,
+				touchEnabled: true,
+		 });
+	  }
+	});
+	
+	
+	$(document).ready(function(){
+		if($(window).width()>1500){
+			$('.assetsslide').bxSlider({
+				infiniteLoop:true,
+				auto:false,
+				slideWidth: 370,
+				maxSlides: 4,
+				pager:false,
+				controls:true,
+				oneToOneTouch:false,
+				pause: 10000,
+				touchEnabled: true,
+				moveSlides:1,
+				 preventDefaultSwipeX:true,
+				preventDefaultSwipeY:true
+			});
+		}
+	   
+		else{
+		if($(window).width()<=1500 && $(window).width() > 1280) {			
+			$('.assetsslide').bxSlider({
+				slideWidth: 290,
+				auto:false,
+				maxSlides: 4,
+				moveSlides:1,
+				pager:false,
+				controls:true,
+				autoHover: true,
+				oneToOneTouch:false,
+				pause: 10000,
+				touchEnabled: true
+			});
+		}
+		
+		else{
+		if($(window).width()<=1280 && $(window).width() > 800) {			
+			$('.assetsslide').bxSlider({
+				slideWidth: 300,
+				auto:false,
+				maxSlides: 4,
+				moveSlides:1,
+				pager:false,
+				controls:true,
+				autoHover: true,
+				oneToOneTouch:false,
+				pause: 10000,
+				touchEnabled: true
+			});
+		}
+		
+		else if($(window).width()<=800) {
+			$('.assetsslide').bxSlider({
+				slideWidth: 400,
+				maxSlides: 1
+				,pager:false,
+				controls:true,
+				autoHover: true,
+				oneToOneTouch:false,
+				pause: 10000,
+				touchEnabled: true
+			});
+		}	   
+	   }
+			}
+	});
+	
+</script>
+	
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.js"></script>
+<script type="text/javascript">
+	
+	$(document).ready(function() {
+		$("data-fancybox").fancybox();
+	});
+	$(document).ready(function() {
+		$(".fancybox").fancybox();
+	});
+</script>
+
+</body>
+</html>
